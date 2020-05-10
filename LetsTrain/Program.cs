291e79448323768534c82201706fd7be4caca5e3
Максим <maxim@s-avt.ru>
2020-05-10 03:24:24 +0300

    Добавьте файлы проекта.

---
+++ LetsTrain/Program.cs
@@ -0,0 +1,44 @@
+﻿using ScreenRecorderLib;
+using System;
+using System.IO;
+using System.Threading;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+namespace LetsTrain
+{
+    class Program
+    {
+        private static readonly SemaphoreSlim RecordingLock = new SemaphoreSlim(0, 1);
+
+        static void Main(string[] args)
+        {
+            using var recorder = Recorder.CreateRecorder(new RecorderOptions() { RecorderMode = RecorderMode.Snapshot });
+            recorder.OnRecordingComplete += Recorder_OnRecordingComplete;
+
+            while (true)
+            {
+                Console.ReadLine();
+
+                recorder.Record(@"D:\Test.png");
+                RecordingLock.Wait();
+
+                ProcessImage();
+            }
+        }
+
+        private static void ProcessImage()
+        {
+            // Сохраняем первую карту
+            using Image image = Image.Load(@"D:\Test.png");
+            image.Mutate(x => x.Crop(new Rectangle(1770, 1350, 195, 135)));
+            image.Save(@"D:\card1.jpg");
+        }
+
+        private static void Recorder_OnRecordingComplete(object sender, RecordingCompleteEventArgs e)
+        {
+            Console.WriteLine("Record complete");
+            RecordingLock.Release();
+        }
+    }
+}
